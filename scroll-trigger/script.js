19a95041b9f1779eb5fda21795ce3b30df8ab664
Pushparaj <psinghparmar42@gmail.com>
2026-08-17 08:36:49 +0530

    add scrolltrigger class

---
+++ scroll-trigger/script.js
@@ -0,0 +1,59 @@
+// gsap.from("#page1 #box", {
+//   scale: 0,
+//   delay: 1,
+//   duration: 2,
+//   rotate: 360,
+// });
+// gsap.to("#page2 #box", {
+//   scale: 0,
+//   delay: 1,
+//   duration: 2,
+//   rotate: 360,
+//   scrollTrigger: {
+//     trigger: "#page2 #box",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//   },
+// });
+// gsap.from("#page3 #box", {
+//   scale: 0,
+//   delay: 1,
+//   duration: 2,
+//   rotate: 720,
+//   scrollTrigger: {
+//     trigger: "#page3 #box",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//     end: "top 30%",
+//     scrub: 2,
+//     pin: true,
+//   },
+// });
+
+// gsap.from("#page2 h1", {
+//   opacity: 0,
+//   duration: 2,
+//   x: 500,
+//   scrollTrigger: {
+//     trigger: "#page2 h1",
+//     scroller: "body",
+//     markers: true,
+//     start: "top 60%",
+//     end: "top 30%",
+//     scrub: 4,
+//   },
+// });
+gsap.to("#page2 h1", {
+  transform: "translateX(-150%)",
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller: "body",
+    markers: true,
+    start: "top 0%",
+    end: "top -150%",
+    scrub: 4,
+    pin: true,
+  },
+});
